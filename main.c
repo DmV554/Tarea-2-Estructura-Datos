0617fcc3b7d74fb9ce6d9576cc0903996efc1811
DmV554 <daniel3318bl@gmail.com>
2023-04-24 02:26:59 +0000

    Archivos

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h> 
+#include "Map.h"
+#include "list.h"
+#include "stack.h"
+
+/*
+  función para comparar claves de tipo string
+  retorna 1 si son iguales
+*/
+int is_equal_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2)==0) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo string
+  retorna 1 si son key1<key2
+*/
+int lower_than_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2) < 0) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo int
+  retorna 1 si son iguales
+*/
+int is_equal_int(void * key1, void * key2) {
+    if(*(int*)key1 == *(int*)key2) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo int
+  retorna 1 si son key1<key2
+*/
+int lower_than_int(void * key1, void * key2) {
+    if(*(int*)key1 < *(int*)key2) return 1;
+    return 0;
+}
+
+
+int main() {
+ 
+    return 0;
+}
+
